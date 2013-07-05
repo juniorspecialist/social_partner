cfad44292822672e3f90f48f3b0e6cc4037c83f7
artem <juniorspecialistphp@gmail.com>
2013-07-05 12:05:02 +0300

    11

---
+++ protected/cron.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: artem
+ * Date: 12.06.13
+ * Time: 16:27
+ * To change this template use File | Settings | File Templates.
+ */
+$yii=dirname(__FILE__).'/../../framework/yii.php';
+$config=dirname(__FILE__).'/config/cron.php';
+
+require_once($yii);
+
+// remove the following line when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+
+Yii::createConsoleApplication($config)->run();
